9dbce6d762c35cf392abdd7c936b2443e3e78ede
xxxk.l.xxx <xxxk.l.xxx@92df3420-62d6-3492-ed7f-eccdc6ab7307>
2010-04-09 10:02:48 +0000

    a little test

--- Games/Dodgem/Product/Dodgem.game.lua
+++ Games/Dodgem/Product/Dodgem.game.lua
@@ -31,6 +31,7 @@ function initialize(game)
 
 	local car1 = game:getWorld():createAgent("Dodgem/Dodgem", "car1", Tanx.RigidBodyState.make(Tanx.Vector3(0, 0.8, 0)))
 	local action = Tanx.VehicleBook.getSingleton():at"Dodgem/Dodgem":make(car1:get():getMainBody())
+	g_Car1Action = action
 
 	g_Driver = action:get().m_deviceStatus:toDerived()
 
@@ -82,6 +83,13 @@ function onStep(elapsed)
 			end
 		end
 	end
+
+	--[[if g_Car1Action then
+		local rpm = g_Car1Action:get():calcRPM()
+		local kmph = g_Car1Action:get():calcKMPH()
+		local mph = g_Car1Action:get():calcMPH()
+		Tanx.log(string.format("[Dodgem\\Dodgem.game.lua]: car1 rpm: %f, kmph: %f, mph: %f.", rpm, kmph, mph))
+	end]]
 end
 
 

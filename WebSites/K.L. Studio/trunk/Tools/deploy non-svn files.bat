fde6a49b5e1c35be0f04723476e7bc1de76941c7
xxxK.L.xxx <k.l.lambda@gmaiol.com>
2010-04-11 11:09:12 +0000

    page of Tetra added

--- WebSites/K.L. Studio/trunk/Tools/deploy non-svn files.bat
+++ WebSites/K.L. Studio/trunk/Tools/deploy non-svn files.bat
@@ -3,3 +3,4 @@ copy	"..\WebRoot\projects\Tanx\Installers\*.msi"		"%~1\projects\Tanx\Installers\
 copy	"..\WebRoot\projects\Tanx\Installers\*.7z"		"%~1\projects\Tanx\Installers\"
 
 copy	"..\WebRoot\projects\Trigo\snapshots\*.png"		"%~1\projects\Trigo\snapshots\"
+copy	"..\WebRoot\projects\Tetra\snapshots\*.png"		"%~1\projects\Tetra\snapshots\"

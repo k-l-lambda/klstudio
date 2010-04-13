e41622e6fd3faffde7f5c4830d4da2528b9559b8
xxxK.L.xxx <k.l.lambda@gmaiol.com>
2010-04-13 15:57:27 +0000

    updated

--- WebSites/K.L. Studio/trunk/Tools/deploy non-svn files.bat
+++ WebSites/K.L. Studio/trunk/Tools/deploy non-svn files.bat
@@ -1,6 +1,7 @@
 
 copy	"..\WebRoot\projects\Tanx\Installers\*.msi"		"%~1\projects\Tanx\Installers\"
 copy	"..\WebRoot\projects\Tanx\Installers\*.7z"		"%~1\projects\Tanx\Installers\"
+copy	"..\WebRoot\projects\Tanx\Installers\*.exe"		"%~1\projects\Tanx\Installers\"
 
 copy	"..\WebRoot\projects\Trigo\snapshots\*.png"		"%~1\projects\Trigo\snapshots\"
 copy	"..\WebRoot\projects\Tetra\snapshots\*.png"		"%~1\projects\Tetra\snapshots\"

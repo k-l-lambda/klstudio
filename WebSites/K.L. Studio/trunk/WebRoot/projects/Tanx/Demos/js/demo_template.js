50b0f991a3ddebf5cf84a1c9dd154e39a51040ed
xxxK.L.xxx <k.l.lambda@gmaiol.com>
2010-04-23 14:24:34 +0000

    updated

--- WebSites/K.L. Studio/trunk/WebRoot/projects/Tanx/Demos/js/demo_template.js
+++ WebSites/K.L. Studio/trunk/WebRoot/projects/Tanx/Demos/js/demo_template.js
@@ -8,34 +8,50 @@
 init = function() {
 	var canvas = document.getElementById("tanx");
 
-	var requestVersion = canvas.attributes["requestVersion"].value;
-	if (tanxjs.util.requiredVersionAvailable(requestVersion)) {
+	var requiredVersion = canvas.attributes["requiredVersion"].value;
+	if (tanxjs.util.requiredVersionAvailable(requiredVersion)) {
 		var pack = canvas.attributes["package"].value;
 		var game = canvas.attributes["game"].value;
 		var envelopePathFormat = canvas.attributes["envelopePathFormat"];
 		var envelope = canvas.attributes["envelope"];
 
 		var inner = ""
-		inner += '<object classid="CLSID:E8C87B51-B729-41bc-8510-404ACB3225B5" style="width: 100%; height: 100%;">';
-		inner += '	<param name="type" value="application/x-klstudio.tanx" />';
-		inner += '	<param name="src" value="' + pack + '" />';
-		inner += '	<param name="game" value="' + game + '" />';
-		if (envelopePathFormat)
-			inner += '	<param name="envelopepathformat" value="' + envelopePathFormat.value + '" />';
-		if (envelope)
-			inner += '	<param name="envelope" value="' + envelope.value + '" />';
-		inner += '</object>';
+		if (tanxjs.util.IsMSIE()) {
+			inner += '<object classid="CLSID:E8C87B51-B729-41bc-8510-404ACB3225B5" style="width: 100%; height: 100%;">';
+			inner += '	<param name="type" value="application/x-klstudio.tanx" />';
+			inner += '	<param name="src" value="' + pack + '" />';
+			inner += '	<param name="game" value="' + game + '" />';
+			if (envelopePathFormat)
+				inner += '	<param name="envelopepathformat" value="' + envelopePathFormat.value + '" />';
+			if (envelope)
+				inner += '	<param name="envelope" value="' + envelope.value + '" />';
+			inner += '</object>';
+		}
+		else {
+			inner += '<embed type="application/x-klstudio.tanx" style="width: 100%; height: 100%;" src="' + pack + '" game="' + game + '"';
+			if (envelopePathFormat)
+				inner += ' envelopepathformat="' + envelopePathFormat.value + '"';
+			if (envelope)
+				inner += ' envelope="' + envelope.value + '"';
+			inner += ' />';
+		}
 
 		canvas.innerHTML = inner;
 	}
 	else {
+		var cover = canvas.attributes["cover"];
 		var subMessage = tanxjs.util.getPluginVersion() ? "This page requires a newer version of the K.L.'s Tanx plugin." : "This page requires the K.L.'s Tanx plugin to be installed.";
-		canvas.innerHTML =
-			'<div style="background: lightblue; width: 100%; height: 100%; text-align:center;">' +
-			'	<img src="res/Cover_Threebody.png" style="width:100%; max-width:640px">' +
+		var inner = '<div style="background: lightblue; width: 100%; height: 100%; text-align:center;">';
+
+		if (cover)
+			inner += '	<img src="' + cover.value + '" style="width:100%; max-width:640px" />';
+
+		inner +=
 			'	<br/><br/>' + subMessage + '<br/>' +
 			'	<a href="../tanx.html">Click here to download.</a>' +
 			'</div>'
 			;
+
+		canvas.innerHTML = inner;
 	}
 };

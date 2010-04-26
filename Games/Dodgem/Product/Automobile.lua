e85cf150e46b49126ce12a2286ebfef68b1cfdd7
xxxk.l.xxx <k.l.lambda@gmaiol.com>
2010-04-26 07:09:45 +0000

    Automobile.lua: class 'AutomobileCollisionListener' added.

--- Games/Dodgem/Product/Automobile.lua
+++ Games/Dodgem/Product/Automobile.lua
@@ -26,6 +26,13 @@ class "Automobile"
 			self.SoundSources.Engine:get():setGain(0)
 			self.SoundSources.Engine:get():play()
 		end
+
+		self.CollisionListener = AutomobileCollisionListener(self.SoundSources)
+		self.Chassis:get():addCollisionListener(self.CollisionListener)
+	end
+
+	function Automobile:__finalize()
+		--self.Chassis:get():removeCollisionListener(self.CollisionListener)
 	end
 
 	function Automobile:step(elapsed)
@@ -43,4 +50,54 @@ class "Automobile"
 			self.SoundSources.Engine:get():setVelocity(velocity.x, velocity.y, velocity.z)
 			self.SoundSources.Engine:get():setGain(self.RPM / 2000)
 		end
+	end
+
+
+class "AutomobileCollisionListener" (Tanx.CollisionListener)
+
+	function AutomobileCollisionListener:__init(sounds)
+		if _LUABIND_VERSION and _LUABIND_VERSION >= 800 then
+			Tanx.CollisionListener.__init(self)
+		else
+			super()
+		end
+
+		self.Sounds = sounds
+	end
+
+	function AutomobileCollisionListener:contactPointAddedCallback(event)
+	end
+
+	function AutomobileCollisionListener:contactPointConfirmedCallback(event)
+		if event.m_collidableB:getShape():getType() ~= Havok.hkpShapeType.BOX then
+			Tanx.log(string.format("AutomobileCollisionListener:contactPointConfirmedCallback: A shape type: %d, B shape type: %d, projected velocity: %f, contace point: %s.",
+				event.m_collidableA:getShape():getType(), event.m_collidableB:getShape():getType(), event.m_projectedVelocity, tostring(Tanx.madp(event.m_contactPoint:getPosition()))))
+		end
+		--[[if self.Sounds then
+			local rba = event.m_collidableA:getRigidBody()
+			local rbb = event.m_collidableB:getRigidBody()
+			if rba and rbb and (rba:isFixed():get() or rbb:isFixed():get()) and event.m_collidableB:getShape():getType() == Havok.hkpShapeType.BOX then
+				local vel = math.abs(event.m_projectedVelocity) - 0.4
+				if vel > 0 then
+					--Tanx.log(string.format("AutomobileCollisionListener:contactPointConfirmedCallback: A fixed: %s, B fixed: %s, projected velocity: %f, contace point: %s.",
+					--	tostring(rba:isFixed():get()), tostring(rbb:isFixed():get()), event.m_projectedVelocity, tostring(Tanx.madp(event.m_contactPoint:getPosition()))))
+
+					local volume = vel * 0.8
+					self.Sounds.BrickCollision:get():setGain(volume)
+					self.Sounds.BrickCollision:get():play()
+				end
+			end
+
+			if event.m_collidableA:getShape():getType() == Havok.hkpShapeType.BOX and event.m_collidableB:getShape():getType() == Havok.hkpShapeType.CONVEX_VERTICES then
+				local volume = math.abs(event.m_projectedVelocity) / 32
+				self.Sounds.GlassCollision:get():setGain(volume)
+				self.Sounds.GlassCollision:get():play()
+			end
+		end]]
+	end
+
+	function AutomobileCollisionListener:contactPointRemovedCallback(event)
+	end
+
+	function AutomobileCollisionListener:contactProcessCallback(event)
 	end

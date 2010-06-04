e547bff09834b565bcbc333d43e5defdb73ea47d
xxxK.L.xxx <k.l.lambda@gmaiol.com>
2010-06-04 17:10:25 +0000

    TailState.lua: state of 'Disabled' added.
    Sound of critical point added.

--- Games/Dodgem/Product/TailState.lua
+++ Games/Dodgem/Product/TailState.lua
@@ -14,7 +14,25 @@ Tanx.require"Core:StateMachine.lua"
 
 local s_TailStateSet =
 {
-	Idle = {},
+	Disabled =
+	{
+		enterState = function(state, machine)
+			machine.Material:get():setDiffuse(machine.Colors.Disabled)
+			machine.Material:get():setAmbient(machine.Colors.Disabled)
+			machine.Material:get():setSpecular(Ogre.ColourValue.Black)
+			machine.Material:get():_notifyNeedsRecompile()
+		end,
+	},
+
+	Active =
+	{
+		enterState = function(state, machine)
+			machine.Material:get():setDiffuse(machine.Colors.Active)
+			machine.Material:get():setAmbient(machine.Colors.Active)
+			machine.Material:get():setSpecular(Ogre.ColourValue.White)
+			machine.Material:get():_notifyNeedsRecompile()
+		end,
+	},
 
 	Flicker =
 	{
@@ -28,12 +46,12 @@ local s_TailStateSet =
 		end,
 
 		step = function(state, machine, elapsed)
-			machine.Material:get():setSelfIllumination(Ogre.ColourValue.White * machine.FlickerBrightness)
+			machine.Material:get():setSelfIllumination(machine.Colors.Flicker * machine.FlickerBrightness)
 			machine.Material:get():_notifyNeedsRecompile()
 
 			machine.FlickerBrightness = machine.FlickerBrightness - elapsed * 1.8
 			if machine.FlickerBrightness <= 0 then
-				machine:switch("Idle", machine)
+				machine:switch("Active", machine)
 			end
 		end
 	},
@@ -42,12 +60,17 @@ local s_TailStateSet =
 
 class "TailState" (TanxStateMachine)
 
-	function TailState:__init(car)
+	function TailState:__init(car, colors)
 		if _LUABIND_VERSION and _LUABIND_VERSION >= 800 then
-			TanxStateMachine.__init(self, s_TailStateSet, "Idle", nil, false)
+			TanxStateMachine.__init(self, s_TailStateSet, nil, nil, false)
 		else
-			super(s_TailStateSet, "Idle", nil, false)
+			super(s_TailStateSet, nil, nil, false)
 		end
+
+		self.Colors = colors or {}
+		self.Colors.Active = self.Colors.Active or Ogre.ColourValue.Red
+		self.Colors.Disabled = self.Colors.Disabled or Ogre.ColourValue(0.4, 0.4, 0.4)
+		self.Colors.Flicker = self.Colors.Disabled or Ogre.ColourValue.White
 
 		self.Material = car:get():findBody"tail":get():getNode():toDerived():getChild(0):toDerived():getAttachedObject(0):toDerived():getSubEntity(0):getMaterial()
 	end
@@ -61,4 +84,12 @@ class "TailState" (TanxStateMachine)
 
 	function TailState:flicker()
 		self:switch("Flicker", self)
+	end
+
+	function TailState:disable()
+		self:switch("Disabled", self)
+	end
+
+	function TailState:activate()
+		self:switch("Active", self)
 	end

cba99d50fbf6ea30aafafabac1040a9eff6eb565
xxxk.l.xxx <xxxk.l.xxx@92df3420-62d6-3492-ed7f-eccdc6ab7307>
2010-06-22 09:50:08 +0000

    LevelConfigs.lua: created.

---
+++ Games/CubeTetris/Product/LevelConfigs.lua
@@ -0,0 +1,36 @@
+--[[
+**	This source file is part of Tanx.
+**
+**	Copyright (c) 2009-2010	K.L.'s Studio	<xxxk.l.xxx@gmail.com>
+**	This program is free software without any warranty.
+
+	LevelConfigs.lua
+--]]
+
+
+Tanx.log("[Tetris\\LevelConfigs.lua]: parsed.")
+
+
+g_DigLevelConfigs =
+{
+	[1] =
+	{
+		BlockLayers = 4,
+		ColorCode = 1,
+		Music = "",
+	},
+
+	[2] =
+	{
+		BlockLayers = 6,
+		ColorCode = 2,
+		Music = "",
+	},
+
+	[3] =
+	{
+		BlockLayers = 8,
+		ColorCode = 3,
+		Music = "",
+	},
+}

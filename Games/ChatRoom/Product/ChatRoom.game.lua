dbff5aa981d55fce6667b9c270c1b5bb80311ef0
xxxK.L.xxx <k.l.lambda@gmaiol.com>
2010-10-29 15:20:33 +0000

    test.html: created.
    ChatRoom.game.lua: created.
    ChatRoom.game: created.
    
    ChangeLog.txt: created.

---
+++ Games/ChatRoom/Product/ChatRoom.game.lua
@@ -0,0 +1,14 @@
+--[[
+**	This source file is part of Tanx.
+**
+**	Copyright (c) 2009-2010	K.L.'s Studio	<xxxk.l.xxx@gmail.com>
+**	This program is free software without any warranty.
+
+	ChatRoom.game.lua
+--]]
+
+Tanx.log("[ChatRoom\\ChatRoom.game.lua]: parsed.")
+
+
+function initialize(game, params)
+end

1ebca0674ba772f3d3bf539a31ddeb59ebe8e545
xxxK.L.xxx <xxxK.L.xxx@92df3420-62d6-3492-ed7f-eccdc6ab7307>
2010-11-01 12:03:02 +0000

    Source/main.py: redirect '\html\.*' to '\.*'.

--- WebSites/K.L. Studio/trunk/Tools/deploy non-svn files.bat
+++ WebSites/K.L. Studio/trunk/Tools/deploy non-svn files.bat
@@ -1,10 +1,10 @@
 
-copy	"..\WebRoot\projects\Tanx\Installers\*.msi"		"%~1\projects\Tanx\Installers\"
-copy	"..\WebRoot\projects\Tanx\Installers\*.7z"		"%~1\projects\Tanx\Installers\"
-copy	"..\WebRoot\projects\Tanx\Installers\*.exe"		"%~1\projects\Tanx\Installers\"
+copy	"..\Source\static\projects\Tanx\Installers\*.msi"		"%~1\projects\Tanx\Installers\"
+copy	"..\Source\static\projects\Tanx\Installers\*.7z"		"%~1\projects\Tanx\Installers\"
+copy	"..\Source\static\projects\Tanx\Installers\*.exe"		"%~1\projects\Tanx\Installers\"
 
-copy	"..\WebRoot\projects\Trigo\snapshots\*.png"		"%~1\projects\Trigo\snapshots\"
-copy	"..\WebRoot\projects\Tetra\snapshots\*.png"		"%~1\projects\Tetra\snapshots\"
+copy	"..\Source\static\projects\Trigo\snapshots\*.png"		"%~1\projects\Trigo\snapshots\"
+copy	"..\Source\static\projects\Tetra\snapshots\*.png"		"%~1\projects\Tetra\snapshots\"
 
-copy	"..\WebRoot\projects\Tanx\ReadMe.html"			"%~1\projects\Tanx\"
-copy	"..\WebRoot\projects\Tanx\Docs\*"				"%~1\projects\Tanx\Docs\"
+copy	"..\Source\static\projects\Tanx\ReadMe.html"			"%~1\projects\Tanx\"
+copy	"..\Source\static\projects\Tanx\Docs\*"					"%~1\projects\Tanx\Docs\"

a3a0e3cd0f07aa421bf65dce32d32c546102ac06
xxxK.L.xxx <xxxK.L.xxx@92df3420-62d6-3492-ed7f-eccdc6ab7307>
2010-11-01 15:31:40 +0000

    Source/TanxWebService: set up.

---
+++ WebSites/K.L. Studio/trunk/Source/TanxWebService/Session.py
@@ -0,0 +1,9 @@
+
+from google.appengine.ext import db
+from google.appengine.api import users
+
+
+class Session:
+    host = db.UserProperty()
+    next_host_message_id = db.IntegerProperty()
+    next_guest_message_id = db.IntegerProperty()
---
+++ WebSites/K.L. Studio/trunk/Source/TanxWebService/main.py
@@ -0,0 +1,41 @@
+
+import os
+import datetime
+import re
+import logging
+
+from google.appengine.ext import db
+from google.appengine.api import users
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import util
+from google.appengine.ext.webapp import template
+
+
+class MainHandler(webapp.RequestHandler):
+    def get(self):
+        self.response.out.write('{result="hello!"}')
+
+    def post(self):
+        for arg in self.request.arguments():
+            logging.info('%s: %s', arg, self.request.get(arg))
+
+
+class UserInfoHandler(webapp.RequestHandler):
+    def get(self):
+        current_user = users.get_current_user()
+        if current_user:
+            self.response.out.write('{nickname="%s",email="%s",user_id="%s"}' % (current_user.nickname(), current_user.email(), current_user.user_id()))
+        else:
+            self.response.out.write('{}')
+
+
+def main():
+    application = webapp.WSGIApplication([
+        ('/tanx-web-service/.*/',                       MainHandler),
+        ('/tanx-web-service/.*/user-info',              UserInfoHandler),
+        ], debug=True)
+    util.run_wsgi_app(application)
+
+
+if __name__ == '__main__':
+    main()

1505274c937b88808947b201848df79e4b58d431
xxxK.L.xxx <k.l.lambda@gmaiol.com>
2010-11-07 09:14:45 +0000

    TanxWebService.SessionPostMessageHandler: implemented. TanxWebService.SessionFetchMessageHandler: implemented.

--- WebSites/K.L. Studio/trunk/Source/TanxWebService/Serializer.py
+++ WebSites/K.L. Studio/trunk/Source/TanxWebService/Serializer.py
@@ -7,17 +7,27 @@ from google.appengine.api import users
 from google.appengine.ext import db
 
 
+class PlainText:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return self.value
+
+
 def save(data):
     t = type(data)
     if t is types.ListType:
         return '{%s}' % string.join([save(elem) for elem in data], ',')
+    if t is types.DictType:
+        return '{%s}' % string.join(['%s=%s' % (key, save(data[key])) for key in data], ',')
     elif (t in [types.StringType, types.UnicodeType]) or isinstance(data, db.Text):
         return '"%s"' % data
     elif t is types.BooleanType:
         return data and 'true' or 'false'
     elif t is types.NoneType:
         return 'nil'
-    elif t in [types.IntType, types.LongType, types.FloatType]:
+    elif t in [types.IntType, types.LongType, types.FloatType] or isinstance(data, PlainText):
         return str(data)
     elif isinstance(data, datetime.datetime):
         return '"%s"' % str(data)

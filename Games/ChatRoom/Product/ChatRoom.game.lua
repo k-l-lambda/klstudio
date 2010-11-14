529409eff0cec2bc65ec4fcedfc79242edb13a2b
xxxK.L.xxx <k.l.lambda@gmaiol.com>
2010-11-14 15:46:03 +0000

    RoomList.layout: created. ChatRoom.layout: created.

--- Games/ChatRoom/Product/ChatRoom.game.lua
+++ Games/ChatRoom/Product/ChatRoom.game.lua
@@ -12,38 +12,92 @@ Tanx.log("[ChatRoom\\ChatRoom.game.lua]: parsed.")
 Tanx.require"Core:WebClient.lua"
 Tanx.require"Core:serializer.lua"
 Tanx.require"Core:bind.lua"
+Tanx.require"Core:CeguiKeyListener.lua"
+Tanx.require"Core:CeguiMouseListener.lua"
 
-
-function reportFinish(result, handler)
-	g_StateLabel:setText(CEGUI.String"Loading finished.")
-
-	Tanx.log("[ChatRoom\\ChatRoom.game.lua]: web result: " .. result)
-
-	if handler then
-		local r = Tanx.serializer.load(result)
-		handler(r)
-		--g_StateLabel:setText(CEGUI.String("result: " .. r.result))
-	end
-end
 
 function reportState(state)
 	g_StateLabel:setText(CEGUI.String("Loading: " .. state))
 end
 
 
+s_WebServiceLocation = "http://localhost:8080/tanx-web-service/v1/"
+s_WebAppLocation = s_WebServiceLocation .. "app/ChatRoom/"
+
+
+function setupRoom()
+	g_StateLabel:setText(CEGUI.String("Seting up room..."))
+
+	local result
+	local entry = g_WebClient:postUrl(s_WebAppLocation .. "setup-session", "Content-Type:\tapplication/x-www-form-urlencoded\n\n", function(response) result = Tanx.serializer.load(response) end, reportState)
+
+	while not entry:get():checkState() do
+		coroutine.yield()
+	end
+
+	--Tanx.log("session-list: " .. archive)
+	g_StateLabel:setText(CEGUI.String"Ready.")
+
+	if result then
+		g_OwnSessionId = result.id
+		Tanx.log("g_OwnSessionId: " .. g_OwnSessionId)
+
+		refreshRoomList()
+	else
+		g_StateLabel:setText(CEGUI.String"Room setup failed.")
+	end
+end
+
+
+function refreshRoomList()
+	g_StateLabel:setText(CEGUI.String("Loading room list..."))
+
+	local result
+	local entry = g_WebClient:getUrl(s_WebAppLocation .. "session-list", function(response) result = Tanx.serializer.load(response) end, reportState)
+
+	while not entry:get():checkState() do
+		coroutine.yield()
+	end
+
+	g_StateLabel:setText(CEGUI.String"Ready.")
+
+	if result then
+		local i, room
+		for i, room in ipairs(result.list) do
+			local listboxitem = CEGUI.ListboxTextItem.new(CEGUI.String(room.host.nickname))
+			listboxitem:setFont(CEGUI.String"BlueHighway-24")
+			listboxitem:setSelectionBrushImage(CEGUI.String"TaharezLook", CEGUI.String"ListboxSelectionBrush")
+			g_RoomList:addItem(listboxitem)
+		end
+	else
+		g_StateLabel:setText(CEGUI.String"Room list loading failed.")
+	end
+end
+
+
 function initialize(game, params)
 	g_Game = game
 
+	g_Keyboard = g_Game:getKeyboard()
+	g_KeyListener = CeguiKeyListener(g_Keyboard)
+	g_Mouse = g_Game:getMouse()
+	g_MouseListener = CeguiMouseListener(g_Mouse)
+
 	-- setup GUI
 	g_GuiSystem = CEGUI.System.getSingleton()
+	g_WindowManager = CEGUI.WindowManager.getSingleton()
 	CEGUI.SchemeManager.getSingleton():loadScheme(CEGUI.String"TaharezLookSkin.scheme")
 	--g_GuiSystem:setDefaultMouseCursor(CEGUI.String"TaharezLook", CEGUI.String"MouseArrow")
 	g_GuiSystem:setDefaultTooltip(CEGUI.String"TaharezLook/Tooltip")
 
-	local sheet = CEGUI.WindowManager.getSingleton():loadWindowLayout(CEGUI.String"Dialog.layout")
+	local sheet = g_WindowManager:loadWindowLayout(CEGUI.String"ChatRoom.layout")
 	g_GuiSystem:setGUISheet(sheet)
 
-	g_StateLabel = CEGUI.WindowManager.getSingleton():getWindow(CEGUI.String"Dialog/State")
+	g_RootWindow = g_WindowManager:getWindow(CEGUI.String"ChatRoom/root")
+	g_StateLabel = g_WindowManager:getWindow(CEGUI.String"ChatRoom/State")
+
+	g_RootWindow:addChildWindow(g_WindowManager:loadWindowLayout(CEGUI.String"RoomList.layout"))
+	g_RoomList = g_WindowManager:getWindow(CEGUI.String"ChatRoom/RoomList/List"):toDerived()
 
 
 	-- setup viewport
@@ -51,20 +105,22 @@ function initialize(game, params)
 	g_MainCamera:setAspectRatio(game:getWindow():getWidth() / game:getWindow():getHeight())
 
 	g_Viewport = game:getWindow():addViewport(g_MainCamera)
-	g_Viewport:setBackgroundColour(Ogre.ColourValue.Black)
+	g_Viewport:setBackgroundColour(Ogre.ColourValue(0.234, 0.382, 0.633))
 
-	g_StateLabel:setText(CEGUI.String"ChatRoom")
+
+	g_StateLabel:setText(CEGUI.String"Ready")
 
 	g_WebClient = g_Game:getWebClient(params)
-	g_LoadEntry = g_WebClient:getUrl("http://localhost:8080/tanx-web-service/v1/user-info", Tanx.bind(reportFinish, Tanx._1, function(data) g_StateLabel:setText(CEGUI.String(string.format("nickname: %s\nemail: %s\nuser_id: %s", data.nickname or "", data.email or "", data.user_id or ""))) end), reportState)
-	--g_LoadEntry = g_WebClient:postUrl("http://localhost:8080/tanx-web-service/v1/", "Content-Type:\tapplication/x-www-form-urlencoded\n\narg=hello;data={a=\"abc\\;def\"}", reportFinish, reportState)
+
+	g_WebThread = coroutine.create(setupRoom)
 end
 
 
 function onStep(elapsed)
-	if g_LoadEntry and g_LoadEntry:get() then
-		if g_LoadEntry:get():checkState() then
-			g_LoadEntry = nil
+	if g_WebThread and coroutine.status(g_WebThread) == "suspended" then
+		local s, e = coroutine.resume(g_WebThread)
+		if not s then
+			error(e)
 		end
 	end
 end

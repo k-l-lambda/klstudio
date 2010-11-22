f911a1ff6ee64deeb66d43eaedfe77411e525251
xxxK.L.xxx <xxxK.L.xxx@92df3420-62d6-3492-ed7f-eccdc6ab7307>
2010-11-22 16:37:23 +0000

    enhanced a little

--- Games/ChatRoom/Product/DialogWindow.lua
+++ Games/ChatRoom/Product/DialogWindow.lua
@@ -9,23 +9,30 @@
 
 Tanx.log("[ChatRoom\\DialogWindow.lua]: parsed.")
 
+Tanx.require"Core:bind.lua"
 Tanx.require"Core:CeguiUtil.lua"
 
+g_functor = function(e) Tanx.log"__onEditTextAccepted__" end
 
 class "DialogWindow"
 
-	function DialogWindow:__init(windowManager, hostname, members)
+	function DialogWindow:__init(windowManager, hostname, members, callbacks)
 		self.WindowManager = windowManager
+
+		self.Callbacks = callbacks or {}
 
 		self.NamePrefix = "Dialog_" .. hostname
 
 		self.FrameWindow = self.WindowManager:loadWindowLayout(CEGUI.String"Dialog.layout", CEGUI.String(self.NamePrefix))
 		self.FrameWindow:setText(CEGUI.String(hostname .. "'s Room"))
 
-		self.MemberList = self.FrameWindow:getChildRecursive(CEGUI.String(self.NamePrefix .. "ChatRoom/Dialog/MemberList")):toDerived()
-		--Tanx.log("self.MemberList: " .. type(self.MemberList))
-
 		self.WindowManager:getWindow(CEGUI.String"ChatRoom/root"):addChildWindow(self.FrameWindow)
+
+		self.MemberList = self.FrameWindow:getChildRecursive(CEGUI.String(self.NamePrefix .. "ChatRoom/Dialog/MemberList")):toDerived()
+		self.EditBox = self.FrameWindow:getChildRecursive(CEGUI.String(self.NamePrefix .. "ChatRoom/Dialog/Edit")):toDerived()
+
+		--g_functor = Tanx.bind(self.onEditTextAccepted, self)
+		self.EditBox:subscribeEvent(CEGUI.Editbox.EventTextAccepted, CEGUI.EventSubscriber(g_functor))
 
 		self.Members = members or {}
 		self:refreshMemberList()
@@ -39,4 +46,13 @@ class "DialogWindow"
 			--listboxitem:setSelectionBrushImage(CEGUI.String"TaharezLook", CEGUI.String"ListboxSelectionBrush")
 			self.MemberList:addItem(listboxitem)
 		end
+	end
+
+	function DialogWindow:onEditTextAccepted()
+		Tanx.log"DialogWindow:onEditTextAccepted"
+		if self.Callbacks.PostMessage then
+			self.Callbacks.PostMessage(self.EditBox:getText():c_str())
+		end
+
+		self.EditBox:setText(CEGUI.String"")
 	end

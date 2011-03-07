510f46100cc6db7efb7e8be7931a7e160ee37f40
xxxk.l.xxx <xxxk.l.xxx@92df3420-62d6-3492-ed7f-eccdc6ab7307>
2011-03-07 06:44:09 +0000

    TanxWebService: cache session data when delete.

--- WebSites/K.L. Studio/trunk/Source/TanxWebService/Application.py
+++ WebSites/K.L. Studio/trunk/Source/TanxWebService/Application.py
@@ -1,10 +1,13 @@
 
 from google.appengine.ext import db
+from google.appengine.api import memcache
 
 from Session import *
 
 
 class Application(db.Model):
+    cache_session_ids = db.StringListProperty()
+
     def id(self):
         return self.key().name()
 
@@ -29,3 +32,26 @@ class Application(db.Model):
         self.delete()
 
         logging.info('application "%s" data deleted.', self.id())
+
+    def cacheSession(self, session):
+        self.cache_session_ids.insert(0, session.id())
+        while len(self.cache_session_ids) > 2000:
+            memcache.delete('SessionData:' + self.cache_session_ids.pop())
+        self.put()
+
+        session_data = {
+            'session': session,
+            'channels': session.channels().fetch(1000),
+            'hostMessages': session.hostMessages().fetch(1000),
+            'guestMessages': session.guestMessages().fetch(1000),
+        }
+
+        memcache.set('SessionData:' + session.id(), session_data)
+
+    def removeCacheSession(self, id):
+        self.cache_session_ids.remove(id)
+        self.put()
+
+        memcache.delete('SessionData:' + id)
+
+        logging.info('cached session "%s" removed.', id)
--- WebSites/K.L. Studio/trunk/Source/TanxWebService/static/admin.js
+++ WebSites/K.L. Studio/trunk/Source/TanxWebService/static/admin.js
@@ -19,3 +19,19 @@ function editTags() {
 	var text = document.getElementById("tags-text");
 	text.value = text.value.match(/\[(.*)\]/)[1].replace(/[u'\s]/g, '');
 }
+
+function loadCachedSessionInfo(app_id) {
+	$(".cached-session").each(function(){
+		var session = $(this);
+		var id = session.find(".session-id").text();
+		$.getJSON("../../../app/" + app_id + "/session/" + id + "/" + "info", function(data){
+			var info = data.info;
+			session.find(".session-host").text(info.host.email);
+			session.find(".session-setup_time").text(info.setup_time);
+			session.find(".session-alive_time").text(info.alive_time);
+			session.find(".session-next_host_message_id").text(info.next_host_message_id);
+			session.find(".session-next_guest_message_id").text(info.next_guest_message_id);
+			session.find(".session-tags").text("[" + info.tags + "]");
+		});
+	});
+}

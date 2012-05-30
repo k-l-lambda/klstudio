d59c4e786e34175869811e1532391f2935201a66
xxxK.L.xxx <xxxK.L.xxx@92df3420-62d6-3492-ed7f-eccdc6ab7307>
2012-05-30 16:53:34 +0000

    Product/PoolScene.lua: createEnvironmentMap added.

--- Games/Pool/Product/PoolScene.lua
+++ Games/Pool/Product/PoolScene.lua
@@ -4,6 +4,8 @@
 
 Tanx.log("[Test\\PoolScene.lua]: parsed.", Ogre.LogMessageLevel.TRIVIAL, false)
 
+Tanx.require"Core:SlottedRenderTargetListener.lua"
+
 
 function makeRigidBodyState(position, orientation, linearVel, angleVel)
 	position = position or Tanx.Vector3.ZERO
@@ -47,14 +49,16 @@ function initialize(world)
 
 	world:createAgent("Pool/Table", "Table", makeRigidBodyState(Tanx.Vector3(0, 5.6, 0)))
 
-	world:createAgent("Pool/Ball_White", "Ball0", makeRigidBodyState(Tanx.Vector3(-8, 6, 0.04), nil, Tanx.Vector3(6, 0, 0)))
+	local wball = world:createAgent("Pool/Ball_White", "Ball0", makeRigidBodyState(Tanx.Vector3(-8, 6, 0.04), nil, Tanx.Vector3(6, 0, 0)))
+	createEnvironmentMap(wball, world)
 
 	local ball = Tanx.UnitConfig(world:getUnitConfig"Pool/Ball_White")
 
 	local i
 	for i = 1, 15 do
 		ball.Nodes:at(0).Appearances:at(0):get():toDerived().MaterialMap:at(0).MaterialName = "Pool/Balls/P" .. i
-		world:createAgent(ball, "Ball%index", makeRigidBodyState(Tanx.Vector3(1, 12 + i * 0.8, 0)))
+		local agent = world:createAgent(ball, "Ball%index", makeRigidBodyState(Tanx.Vector3(1, 12 + i * 0.8, 0)))
+		createEnvironmentMap(agent, world)
 	end
 
 	local camera = world:createCamera"Default"
@@ -63,4 +67,41 @@ function initialize(world)
 	camera:setPosition(Tanx.Vector3(23.3, 16, 28.18))
 	camera:lookAt(Tanx.Vector3(0, 6, 0))
 	world:setCurrentCamera"Default"
+
+	g_MainCamera = camera
+end
+
+
+g_Listeners = {}
+
+function createEnvironmentMap(agent, world)
+	local agentname = agent:get():getName()
+
+	local camera = world:createCamera(agentname .. "_camera")
+	camera:setNearClipDistance(0.1)
+	camera:setFOVy(Ogre.Radian(math.pi * 0.49))
+	camera:setAspectRatio(1)
+
+	local entity = agent:get():getNode():getChild(0):getChild(0):toDerived():getAttachedObject(0):toDerived()
+
+	local texname = agentname .. "_envmap"
+	local tex = Ogre.TextureManager.getSingleton():createManual2(texname, Ogre.ResourceGroupManager.DEFAULT_RESOURCE_GROUP_NAME, Ogre.TextureType['2D'], 256, 256, 0, Ogre.PixelFormat.R8G8B8, Ogre.TextureUsage.RENDERTARGET)
+	local target = tex:get():getBuffer():get():getRenderTarget()
+	target:addViewport(camera):setMaterialScheme"Spherical"
+	local l = Tanx.SlottedRenderTargetListener{
+		preRenderTargetUpdate = function()
+			entity:setVisible(false)
+
+			camera:setPosition(agent:get():getMainBody():get():getPosition())
+			camera:lookAt(g_MainCamera:getDerivedPosition())
+		end,
+		postRenderTargetUpdate = function()
+			entity:setVisible(true)
+		end,
+	}
+	target:addListener(l)
+	table.insert(g_Listeners, l)
+
+	local material = entity:getSubEntity(0):getMaterial()
+	material:get():getTechnique(0):getPass"Env":getTextureUnitState(0):setTextureName(texname)
 end

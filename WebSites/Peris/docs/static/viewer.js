7a5adcc41f753783668c94178d4dcf892b78a16c
xxxk.l.xxx@gmail.com <xxxk.l.xxx@gmail.com@92df3420-62d6-3492-ed7f-eccdc6ab7307>
2015-01-29 09:35:01 +0000

    static/viewer.js: added slot load/unload.

--- WebSites/Peris/docs/static/viewer.js
+++ WebSites/Peris/docs/static/viewer.js
@@ -12,14 +12,29 @@ Viewer.prototype.ColumnBottom = [];
 Viewer.prototype.SlotColumn = 5;
 Viewer.prototype.SlotGap = 0.04;
 
+Viewer.prototype.CacheHeight = 1600;
+
 
 Viewer.prototype.initialize = function () {
 	this.Container.addClass("viewer");
 
 	var viewer = this;
 
+	this.OldScrollTop = this.Container.parent().scrollTop();
+
 	this.Container.parent().scroll(function () {
 		viewer.updateLayout();
+
+		viewer.OldScrollTop = viewer.Container.parent().scrollTop();
+	});
+
+	this.StyleTag = $("<style class='viewer-onfly-style'></style>");
+	this.StyleTag.appendTo("body");
+
+	this.updateStyle();
+
+	this.Container.on("appear", ".slot.blank", function (e, slot) {
+		viewer.loadSlot(slot);
 	});
 };
 
@@ -32,6 +47,10 @@ Viewer.prototype.update = function (data) {
 
 	//this.laySlots(data.length);
 	this.updateLayout();
+};
+
+Viewer.prototype.updateStyle = function () {
+	this.StyleTag.text(".slot { width: " + (((1 - this.SlotGap) / this.SlotColumn) * 100).toFixed(2) + "%; }");
 };
 
 Viewer.prototype.clear = function (data) {
@@ -47,31 +66,28 @@ Viewer.prototype.clear = function (data) {
 Viewer.prototype.newSlot = function (path) {
 	var slot = $("<div class='slot new' data-path='" + path + "'><div>");
 	slot.css({
-		height: "200px",
-		width: (((1 - this.SlotGap) / this.SlotColumn) * 100).toFixed(2) + "%",
-		margin: (this.SlotGap * 0.5 / this.SlotColumn * 100).toFixed(2) + "%"
+		height: "200px"
 	});
-
-	var img = $("<img class='figure' src='/images/" + encodeURI(path) + "' alt='" + path + "' />");
-	img.appendTo(slot);
 
 	var viewer = this;
 
-	img.load(function () {
+	var onload = function () {
 		var slot = $(event.currentTarget).parent();
 
 		slot.css({ height: "auto" });
 
 		viewer.mountSlot(slot);
-	});
+	};
 
-	img.error(function () {
+	var onerror = function () {
 		var slot = $(event.currentTarget).parent();
 
 		slot.css({ height: "4em" });
 
 		viewer.mountSlot(slot);
-	});
+	};
+
+	this.loadSlot(slot, onload, onerror);
 
 	return slot;
 };
@@ -92,6 +108,33 @@ Viewer.prototype.getSlot = function (index) {
 	return $(this.Container.find(".slot")[index]);
 };
 
+Viewer.prototype.loadSlot = function (slot, onload, onerror) {
+	var path = slot.data("path");
+
+	var img = $("<img class='figure' src='/images/" + encodeURI(path) + "' alt='" + path + "' />");
+	img.appendTo(slot);
+
+	if (onload)
+		img.load(onload);
+
+	if (onerror)
+		img.error(onerror);
+
+	slot.removeClass("blank");
+	slot.addClass("filled");
+};
+
+Viewer.prototype.unloadSlot = function (slot) {
+	slot.css({ height: slot.height() + "px" });
+
+	slot.find(".figure").remove();
+
+	slot.removeClass("filled");
+	slot.addClass("blank");
+
+	slot.appear();
+};
+
 Viewer.prototype.mountSlot = function (slot) {
 	var shortest = 0;
 	for (var i = 0; i < this.SlotColumn; ++i) {
@@ -103,7 +146,7 @@ Viewer.prototype.mountSlot = function (slot) {
 	slot.css({
 		top: (this.ColumnBottom[shortest] + 10).toFixed(0) + "px",
 		left: ((shortest + this.SlotGap) * 100 / this.SlotColumn).toFixed(2) + "%",
-		height: slot.find(".figure").height().toFixed(0) + "px"
+		height: "auto"
 	});
 
 	slot.removeClass("new");
@@ -126,13 +169,45 @@ Viewer.prototype.slotCompleted = function () {
 };
 
 Viewer.prototype.updateLayout = function () {
+	var readyBottom = this.getReadyBottom();
+	var scrollTop = this.Container.parent().scrollTop();
+
 	if (!this.slotCompleted()) {
 		var newSlotCount = this.Container.find(".slot.new").length;
-		if (this.Container.parent().scrollTop() + this.Container.parent().height() > this.getReadyBottom()) {
+		if (scrollTop + this.Container.parent().height() > readyBottom) {
 			while (newSlotCount < this.SlotColumn * 5) {
 				this.laySlots(this.SlotColumn);
 				newSlotCount += this.SlotColumn;
 			}
 		}
 	}
+
+	var upBound = scrollTop - this.CacheHeight;
+	var bottomBound = scrollTop + this.Container.parent().height() + this.CacheHeight;
+
+	var viewer = this;
+
+	// unload outer slot
+	this.Container.find(".slot.ready.filled").each(function (i, slot) {
+		var $slot = $(slot);
+
+		var top = $slot.position().top;
+		var bottom = top + $slot.height();
+
+		if (bottom < upBound || top > bottomBound)
+			viewer.unloadSlot($slot);
+	});
+
+	/*// load inner slot
+	this.Container.find(".slot.ready.blank").each(function (i, slot) {
+	var $slot = $(slot);
+
+	var top = $slot.position().top;
+	var bottom = top + $slot.height();
+
+	if (bottom >= upBound || top <= bottomBound)
+	viewer.loadSlot($slot);
+	});*/
+	if (scrollTop < this.OldScrollTop || bottomBound < readyBottom)
+		$.force_appear();
 };

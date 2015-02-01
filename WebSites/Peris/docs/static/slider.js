e8d274c546b4f0f5465b31fe889d7985584a6694
xxxk.l.xxx@gmail.com <xxxk.l.xxx@gmail.com@92df3420-62d6-3492-ed7f-eccdc6ab7307>
2015-02-01 18:47:04 +0000

    static/slider.js: figure switching implemented.

--- WebSites/Peris/docs/static/slider.js
+++ WebSites/Peris/docs/static/slider.js
@@ -8,14 +8,22 @@ Peris.Slider = function (viewer) {
 	this.initialize();
 };
 
-Peris.Peer.prototype.Showing = false;
-Peris.Peer.prototype.CurrentIndex = 0;
-Peris.Peer.prototype.PathList = [];
+Peris.Slider.prototype.Showing = false;
+Peris.Slider.prototype.CurrentIndex = 0;
+Peris.Slider.prototype.PathList = [];
+Peris.Slider.prototype.SwitchDuration = 800;
+Peris.Slider.prototype.FastSwitchDuration = 300;
+Peris.Slider.prototype.SwitchInterval = 4000;
+Peris.Slider.prototype.SwitchHandle = null;
+Peris.Slider.prototype.Switching = false;
 
 
 Peris.Slider.prototype.initialize = function () {
 	this.Panel = $("<div class='slider fullscreen-panel'>"
 		+ "<button class='exit'></button>"
+		+ "<div class='prev'></div>"
+		+ "<div class='next'></div>"
+		+ "<div class='current'></div>"
 		+ "</div>");
 
 	this.Panel.appendTo("body");
@@ -29,15 +37,42 @@ Peris.Slider.prototype.initialize = function () {
 	});
 
 	this.Panel.find(".exit").click(function () { slider.close(); });
+
+	this.Prev = this.Panel.find(".prev");
+	this.Next = this.Panel.find(".next");
+	this.Current = this.Panel.find(".current");
 };
 
-Peris.Slider.prototype.open = function (startIndex) {
-	console.log("Slider open at", startIndex);
+Peris.Slider.prototype.open = function (options) {
+	options = options || {};
+	options.startIndex = options.startIndex || 0;
 
-	this.CurrentIndex = startIndex;
+	console.log("Slider open at", options.startIndex);
+
+	this.CurrentIndex = options.startIndex;
 	this.PathList = this.Viewer.PathList;
+	this.OnClose = options.onClose;
 
 	this.Panel.fadeIn();
+
+	var slider = this;
+
+	this.OldOnmouseWheel = document.onmousewheel;
+	document.onmousewheel = function () {
+		slider.onMouseWheel(event);
+	};
+
+	this.Prev.empty();
+	this.Next.empty();
+	this.Current.empty();
+
+	this.Prev.append(this.newFigure(this.CurrentIndex - 1));
+	this.Current.append(this.newFigure(this.CurrentIndex));
+	this.Next.append(this.newFigure(this.CurrentIndex + 1));
+
+	this.Current.find(".figure").show();
+
+	this.startSwitching();
 
 	this.Showing = true;
 };
@@ -45,14 +80,144 @@ Peris.Slider.prototype.open = function (startIndex) {
 Peris.Slider.prototype.close = function () {
 	this.Panel.fadeOut();
 
+	document.onmousewheel = this.OldOnmouseWheel;
+
+	this.stopSwitching();
+
 	this.Showing = false;
+
+	if (this.OnClose)
+		this.OnClose();
+};
+
+Peris.Slider.prototype.startSwitching = function () {
+	var slider = this;
+
+	this.stopSwitching();
+
+	this.SwitchHandle = setInterval(function () {
+		slider.next();
+	}, this.SwitchInterval);
+};
+
+Peris.Slider.prototype.stopSwitching = function () {
+	if (this.SwitchHandle) {
+		clearInterval(this.SwitchHandle);
+		this.SwitchHandle = null;
+	}
+};
+
+Peris.Slider.prototype.prev = function (duration) {
+	if (this.Switching)
+		return;
+
+	duration = duration || this.SwitchDuration;
+
+	var slider = this;
+
+	this.Switching = true;
+
+	this.Current.find(".figure").fadeOut(duration);
+	this.Prev.find(".figure").fadeIn(duration, function () {
+		var f1 = slider.Current.find(".figure");
+		var f2 = slider.Prev.find(".figure");
+
+		slider.Next.empty();
+
+		f1.detach();
+		f1.appendTo(slider.Next);
+
+		f2.detach();
+		f2.appendTo(slider.Current);
+
+		slider.Prev.append(slider.newFigure(slider.CurrentIndex - 1));
+
+		slider.Switching = false;
+
+		slider.onSwitched();
+	});
+
+	--this.CurrentIndex;
+};
+
+Peris.Slider.prototype.next = function (duration) {
+	if (this.Switching)
+		return;
+
+	duration = duration || this.SwitchDuration;
+
+	var slider = this;
+
+	this.Switching = true;
+
+	this.Current.find(".figure").fadeOut(duration);
+	this.Next.find(".figure").fadeIn(duration, function () {
+		var f1 = slider.Current.find(".figure");
+		var f2 = slider.Next.find(".figure");
+
+		slider.Prev.empty();
+
+		f1.detach();
+		f1.appendTo(slider.Prev);
+
+		f2.detach();
+		f2.appendTo(slider.Current);
+
+		slider.Next.append(slider.newFigure(slider.CurrentIndex + 1));
+
+		slider.Switching = false;
+
+		slider.onSwitched();
+	});
+
+	++this.CurrentIndex;
 };
 
 Peris.Slider.prototype.currentPath = function () {
 	return this.PathList[this.CurrentIndex];
 };
 
+Peris.Slider.prototype.newFigure = function (index, onFinish) {
+	index = Math.min(index, this.PathList.length - 1);
+	index = Math.max(index, 0);
+
+	var path = this.PathList[index];
+	var figure = $("<img class='figure' src=\"/images/" + encodeURI(path) + "\" alt='" + path + "'>");
+	figure.hide();
+
+	figure.load(function () {
+		var figure = $(this);
+		var vertical = this.naturalHeight / this.naturalWidth > $(window).height() / $(window).width();
+
+		figure.addClass(vertical ? "vertical-full" : "horizontal-full");
+
+		if (onFinish)
+			onFinish(true);
+	});
+
+	figure.error(function () {
+		if (onFinish)
+			onFinish(false);
+	});
+
+	return figure;
+};
+
 Peris.Slider.prototype.onClick = function (e) {
+	if (this.Showing) {
+		this.next(this.FastSwitchDuration);
+
+		e.preventDefault();
+	}
+};
+
+Peris.Slider.prototype.onMouseWheel = function (e) {
+	if (this.Showing) {
+		if (e.wheelDelta > 0)
+			this.prev(this.FastSwitchDuration);
+		else if (e.wheelDelta < 0)
+			this.next(this.FastSwitchDuration);
+	}
 };
 
 Peris.Slider.prototype.onKeyDown = function (e) {
@@ -72,4 +237,11 @@ Peris.Slider.prototype.onKeyDown = function (e) {
 		if (handled)
 			e.preventDefault();
 	}
+};
+
+Peris.Slider.prototype.onSwitched = function () {
+	var slots = this.Viewer.SlotStream.find(".slot");
+	var index = Math.min(this.CurrentIndex, slots.length - 1);
+
+	this.Viewer.focusSlot($(slots[index]));
 };

358a26f693770e1fdebe5e9ed7c8c17e4295196b
k-l-lambda <k.l.lambda@gmail.com>
2015-03-13 14:54:54 +0800

    replaced google code URL.

--- WebSites/K.L. Studio/trunk/Tools/Publish -dropbox.bat
+++ WebSites/K.L. Studio/trunk/Tools/Publish -dropbox.bat
@@ -1,5 +1,5 @@
 
-call	svn	export --force	"https://klstudio.googlecode.com/svn/WebSites/K.L. Studio/trunk/WebRoot"	"%KLSTUDIO_PUBLISH_DIR%"
+call	svn	export --force	"https://github.com/k-l-lambda/klstudio/trunk/WebSites/K.L. Studio/trunk/WebRoot"	"%KLSTUDIO_PUBLISH_DIR%"
 
 call	"deploy non-svn files.bat"	"%KLSTUDIO_PUBLISH_DIR%"
 
--- WebSites/K.L. Studio/trunk/Tools/Publish.bat
+++ WebSites/K.L. Studio/trunk/Tools/Publish.bat
@@ -1,5 +1,5 @@
 
-call	svn	export --force	"https://klstudio.googlecode.com/svn/WebSites/K.L. Studio/trunk/Source"	"..\release"
+call	svn	export --force	"https://github.com/k-l-lambda/klstudio/trunk/WebSites/K.L. Studio/trunk/Source"	"..\release"
 call	"deploy non-svn files.bat"	"..\release\static"
 
 appcfg.py --email=xxxk.l.xxx@gmail.com update "..\\release\\"

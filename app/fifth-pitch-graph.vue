cf40df15e2e24fd4337bbe1318fc6bf24a842ece
k-l-lambda <k.l.lambda@gmail.com>
2018-12-31 11:26:52 +0800

    fifth-pitch-graph.vue: added steps.

--- app/fifth-pitch-graph.vue
+++ app/fifth-pitch-graph.vue
@@ -12,6 +12,7 @@
 			<line :y1="-100" :y2="100" :x1="0" :x2="0" />
 		</g>
 		<g class="band">
+			<path class="bg" :d="`M-100 ${CONTOUR_SLOPE * 100} L100 ${CONTOUR_SLOPE * -100} L101 ${CONTOUR_SLOPE * -100} L-99 ${CONTOUR_SLOPE * 100} Z`" />
 			<line class="contour-1" :x1="-100" :y1="CONTOUR_SLOPE * 100" :x2="100" :y2="CONTOUR_SLOPE * -100" />
 			<line class="contour-2" :x1="-99" :y1="CONTOUR_SLOPE * 100" :x2="101" :y2="CONTOUR_SLOPE * -100" />
 		</g>
@@ -20,9 +21,18 @@
 				:transform="`translate(${point.m}, ${-point.n})`"
 				:class="{emphasize: point.onBand}"
 			>
-				<circle class="dot" />
-				<text class="label" v-if="point.label" :font-size="point.onBand ? 0.24 : 0.16" transform="translate(0, -0.1)">{{point.label}}</text>
+				<circle class="dot" :class="{axis: point.m == 0 || point.n == 0}" />
+				<text class="label" v-if="point.label" :font-size="point.onBand ? 0.24 : 0.16" :transform="`translate(${point.m == 0 ? 0.14 : 0}, -0.1)`">{{point.label}}</text>
 			</g>
+		</g>
+		<g class="steps">
+			<line v-for="step of steps" :key="`step-line-${step.n}`"
+				:x1="-100 + step.m" :y1="CONTOUR_SLOPE * 100 - step.n" :x2="100 + step.m" :y2="CONTOUR_SLOPE * -100 - step.n"
+			/>
+			<text v-for="step of steps" :key="`step-text-${step.n}`"
+				:transform="`translate(${step.m - 0.2}, ${-step.n + 0.4})`"
+				:font-size="0.4"
+			>{{step.name}}</text>
 		</g>
 	</SvgMap>
 </template>
@@ -65,6 +75,21 @@
 						onBand: value >= 1 && value < 2,
 					})))),
 				CONTOUR_SLOPE,
+				steps: [
+					{m: 0, n: 0, name: "C"},
+					{m: -1, n: 1, name: "G"},
+					{m: -3, n: 2, name: "D"},
+					{m: -4, n: 3, name: "A"},
+					{m: -6, n: 4, name: "E"},
+					{m: -7, n: 5, name: "B"},
+					{m: -9, n: 6, name: "\u266fF"},
+					{m: 2, n: -1, name: "F"},
+					{m: 4, n: -2, name: "\u266dB"},
+					{m: 5, n: -3, name: "\u266dE"},
+					{m: 7, n: -4, name: "\u266dA"},
+					{m: 8, n: -5, name: "\u266dD"},
+					{m: 10, n: -6, name: "\u266dG"},
+				],
 			};
 		},
 
@@ -81,10 +106,12 @@
 
 
 			pointLabel(m, n) {
-				if (Math.abs(m) > 12 || Math.abs(n) > 12)
+				if (Math.abs(m) > 19 || Math.abs(n) > 12)
 					return null;
 
-				if (m >= 0 && n >= 0)
+				if (Math.max(Math.abs(m), Math.abs(n)) > 9)
+					return ((2 ** m) * (3 ** n)).toPrecision(4);
+				else if (m >= 0 && n >= 0)
 					return (2 ** m) * (3 ** n);
 				else if (m < 0 && n < 0)
 					return `1 / ${(2 ** -m) * (3 ** -n)}`;
@@ -104,6 +131,11 @@
 		height: 100%;
 	}
 
+	.svg-map text
+	{
+		font-family: Arial;
+	}
+
 	.axes
 	{
 		stroke: black;
@@ -119,7 +151,12 @@
 	.points .emphasize .dot
 	{
 		r: 0.04;
-		fill: green;
+		fill: #fbab00;
+	}
+
+	.points .dot.axis
+	{
+		r: 0.06;
 	}
 
 	.points .label
@@ -131,6 +168,11 @@
 	.points .emphasize .label
 	{
 		font-weight: bold;
+	}
+
+	.band .bg
+	{
+		fill: #3362;
 	}
 
 	.band line
@@ -146,5 +188,19 @@
 	.contour-2
 	{
 		stroke: steelblue;
+	}
+
+	.steps text
+	{
+		font-weight: bold;
+		text-anchor: middle;
+		fill: #fbab00;
+		user-select: none;
+	}
+
+	.steps line
+	{
+		stroke: #a60;
+		stroke-width: 0.001;
 	}
 </style>

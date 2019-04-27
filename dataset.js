91fdb34add4c0b40fdd3941c7cc289f6e76b3df4
k-l-lambda <k.l.lambda@gmail.com>
2019-04-27 19:03:54 +0800

    dataset.js: updateItem added.

--- dataset.js
+++ dataset.js
@@ -1,11 +1,21 @@
 
+import path from "path";
+
 import * as peris from "./perisDb.js";
 
 
 
 const test = async () => {
-	const f1 = await peris.files.fetch({where: "hash = '0011d8f2a2a2b8c7e0996430b88d12a7'"});
-	console.log("test:", f1);
+	updateItem("0011d8f2a2a2b8c7e0996430b88d12a7");
+};
+
+
+const updateItem = async id => {
+	const file = (await peris.files.fetch({where: `hash = '${id}'`}))[0];
+	if (!file)
+		throw new Error("file not found");
+
+	console.log("path:", path.resolve(process.env.RAW_DATA_PATH, file.path));
 };
 
 

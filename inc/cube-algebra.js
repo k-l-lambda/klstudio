3905f39a76aa363ed33720add5b91e3b832ae465
k-l-lambda <k.l.lambda@gmail.com>
2019-06-22 23:39:03 +0800

    cube-algebra.js: item reduce methods implemented.

--- inc/cube-algebra.js
+++ inc/cube-algebra.js
@@ -1,4 +1,9 @@
 
+const _i = 0;
+const _j = 1;
+const _k = 2;
+
+
 class Item {
 	constructor (unit, exponent) {
 		this.unit = unit;
@@ -11,7 +16,7 @@ class Item {
 
 		switch (this.exponent) {
 		case 0:
-			return "1";
+			return "";
 
 		case -1:
 			postfix = "'";
@@ -70,9 +75,29 @@ class Item {
 
 	static sqauredReduce (item1, item2) {
 		if (item1.exponent === 2 && item2.exponent === 2)
-			return [new Item(this.supplementaryUnit(item1, item2), 2)];
+			return new Item(this.supplementaryUnit(item1, item2), 2);
 
-		return [item1, item2];
+		return null;
+	}
+
+
+	static exchangeReduce (item1, item2) {
+		if (item1.unit === _i)
+			return null;
+
+		if (item1.unit === item2.unit)
+			return null;
+
+		const obey = (item2.unit - item1.unit + 3) % 3 === 1;
+		const u2 = obey ? _j : _k;
+
+		const otherExponent = item1.exponent * item2.exponent * (obey ? 1 : -1);
+		const otherUnit = Item.supplementaryUnit(item1, item2);
+		const otherItem = new Item(otherUnit, otherExponent);
+
+		const exps = [item1, item2, otherItem].reduce((map, item) => (map[item.unit] = item.exponent, map), []);
+
+		return [new Item(_i, exps[_i]), new Item(u2, exps[u2])];
 	}
 };
 
@@ -92,10 +117,12 @@ class Orientation {
 
 
 	normalize () {
+		//console.log("step0:", this.toString());
+
 		this.items = this.items.map(item => item.normalized());
 
 		// expand squred items
-		for (let i = 1; i < this.items.length; ++i) {
+		for (let i = 0; i < this.items.length; ++i) {
 			if (this.items[i].exponent === 2) {
 				const unit = new Item(this.items[i].unit, 1);
 				this.items.splice(i, 1, unit, unit);
@@ -103,8 +130,25 @@ class Orientation {
 				++i;
 			}
 		}
+		//console.log("step1:", this.toString());
 
 		// substitute by exchange formulas
+		while (true) {
+			let reduced = null;
+			for (let i = 1; i < this.items.length; ++i) {
+				reduced = Item.exchangeReduce(this.items[i - 1], this.items[i]);
+				if (reduced) {
+					//console.log("exchange reduced:", new Orientation([this.items[i - 1], this.items[i]]).toString(), new Orientation(reduced).toString());
+
+					this.items.splice(i - 1, 2, ...reduced);
+					break;
+				}
+			}
+
+			if (!reduced)
+				break;
+		}
+		//console.log("step2:", this.toString());
 
 		// merge continous units
 		for (let i = 1; i < this.items.length; ++i) {
@@ -113,21 +157,21 @@ class Orientation {
 				--i;
 			}
 		}
+		//console.log("step3:", this.toString());
 
-		this.items = this.items.map(item => item.normalized());
+		this.items = this.items.map(item => item.normalized()).filter(item => item.exponent !== 0);
 
 		// substitute by squared formulas
-		if (this.items.length >= 2)
-			this.items.splice(0, 2, ...Item.sqauredReduce(...this.items));
+		if (this.items.length >= 2) {
+			const reduced = Item.sqauredReduce(...this.items);
+			if (reduced)
+				this.items.splice(0, 2, reduced);
+		}
 
 		return this;
 	}
 };
 
-
-const _i = 0;
-const _j = 1;
-const _k = 2;
 
 const I = new Item(_i, 1);
 const I_ = new Item(_i, -1);

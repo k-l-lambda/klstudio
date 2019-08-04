c1c1059596dcbf029b6b3496d10a43eba5122170
k-l-lambda <k.l.lambda@gmail.com>
2019-08-04 18:15:37 +0800

    main.py: model added.

--- changelog.md
+++ changelog.md
@@ -2,6 +2,18 @@
 
 *	2019.8.4	K.L.	<k.l.lambda@gmail.com>
 
+	*	main.py: model added.
+
+	*	Model topology experiment:
+
+		|	topology			|	10 batch loss	|
+		| --------------------- | ----------------- |
+		|	cylinder 2 layers	|	0.0648			|
+		|	cylinder 5 layers	|	0.0051			|
+		|	cylinder 9 layers	|	0.0065			|
+		|	cylinder 18 layers	|	0.0099			|
+		|	funnel 9 layers		|	0.0111			|
+
 	*	main.py created.
 
 	*	Project setup.
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import sys
 
 import numpy as np
+import tensorflow as tf
 
 
 
@@ -14,10 +15,44 @@ def readBytesFile(filename, shape):
 
 
 def main(argv):
-	dataLength = readBytesFile('./data/cube3-solver-length.data', (-1, 1))
-	dataState = readBytesFile('./data/cube3-solver-states.data', (-1, 24 * 26))
+	STATE_SIZE = 24 * 26
 
-	print('read:', dataLength.shape, dataState.shape)
+	dataLength = readBytesFile('./data/cube3-solver-length.data', (-1, 1))
+	dataState = readBytesFile('./data/cube3-solver-states.data', (-1, STATE_SIZE))
+
+	#print('read:', dataLength.shape, dataState.shape)
+	# 0.0111
+	'''	tf.keras.layers.Dense(24 * 13, activation = 'relu', input_shape = (STATE_SIZE,)),
+		tf.keras.layers.Dense(12 * 13, activation = 'relu'),
+		tf.keras.layers.Dense(12 * 13, activation = 'relu'),
+		tf.keras.layers.Dense(12 * 13, activation = 'relu'),
+		tf.keras.layers.Dense(12 * 13, activation = 'relu'),
+		tf.keras.layers.Dense(12 * 13, activation = 'relu'),
+		tf.keras.layers.Dense(12 * 13, activation = 'relu'),
+		tf.keras.layers.Dense(13, activation = 'relu'),'''
+	# tf.keras.layers.Dense(STATE_SIZE, activation = 'relu') * 9
+	# 0.0065
+	# tf.keras.layers.Dense(STATE_SIZE, activation = 'relu') * 2
+	# 0.0648
+	# tf.keras.layers.Dense(STATE_SIZE, activation = 'relu') * 5
+	# 0.0051
+	# tf.keras.layers.Dense(STATE_SIZE, activation = 'relu') * 18
+	# 0.0099
+	model = tf.keras.Sequential([
+		tf.keras.layers.Dense(STATE_SIZE, activation = 'relu', input_shape = (STATE_SIZE,)),
+		tf.keras.layers.Dense(12 * 13, activation = 'relu'),
+		tf.keras.layers.Dense(12 * 13, activation = 'relu'),
+		tf.keras.layers.Dense(12 * 13, activation = 'relu'),
+		tf.keras.layers.Dense(1),
+	])
+
+	model.compile(
+		optimizer = tf.keras.optimizers.SGD(lr = 0.01),
+		loss = 'mean_squared_error',
+		metrics = ['accuracy']
+	)
+
+	model.fit(dataState, dataLength, epochs = 10, batch_size = 32)
 
 
 

900b004e7efc649b08cdb23127724d16062463ce
k.l.lambda <k.l.lambda@gmail.com>
2019-08-05 19:03:07 +0800

    cube3-solver.vue: added cube3 player.

--- app/cube3-solver.vue
+++ app/cube3-solver.vue
@@ -1,14 +1,22 @@
 <template>
 	<body>
+		<Cube3Player />
 	</body>
 </template>
 
 <script>
 	import * as tf from "@tensorflow/tfjs";
 
+	import Cube3Player from "./cube3-player.vue";
+
 
 	export default {
 		name: "cube3-solver",
+
+
+		components: {
+			Cube3Player,
+		},
 
 
 		mounted () {
@@ -20,9 +28,22 @@
 			async loadModel () {
 				this.model = await tf.loadLayersModel("/mlmodels/cube3/length/model.json");
 			},
+
+
+			solve () {
+				// TODO:
+			},
 		},
 	};
 </script>
 
 <style>
+	body
+	{
+		position: absolute;
+		width: 100vw;
+		height: 100vh;
+		margin: 0;
+		overflow: hidden;
+	}
 </style>

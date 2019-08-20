0ea9e72887774cc4dc54e63742cac61f54a4d632
k.l.lambda <k.l.lambda@gmail.com>
2019-08-20 14:32:23 +0800

    cube3-gen-loop.ts: statistics table added.

--- tools/cube3-gen-loop.ts
+++ tools/cube3-gen-loop.ts
@@ -13,6 +13,8 @@ const table: { [key: string] : string; } = JSON.parse(tableContent.toString());
 //const states = Object.entries(table).filter(([state, path]) => path.length <= DEPTH).map(([state, path]) => state);
 const states = Object.keys(table);
 
+console.log("table-6 loaded.");
+
 
 const isOrigin = cube => cube.units.reduce((sum, unit) => sum + unit, 0) === 0;
 
@@ -27,7 +29,7 @@ const cubeLoop = cube => {
 
 
 const loopTable: { [key: string] : number; } = {};
-const loopSet = new Set();
+//const loopSet = new Set();
 
 for (let state of states) {
 	if (table[state].length > DEPTH)
@@ -38,12 +40,44 @@ for (let state of states) {
 	loopTable[table[state]] = loop
 	//console.log(table[state], loop);
 
-	loopSet.add(loop);
+	//loopSet.add(loop);
 }
 
-console.log("loopSet:", loopSet);
+//console.log("loopSet:", loopSet);
+console.log("loopTable finished.");
 
-fs.writeFile("./cube-loop.json", JSON.stringify(loopTable), error => console.log("output file write finished:", error));
+
+const loopStatistics = loopTable => {
+	const loopEntries = Object.entries(loopTable);
+
+	/*const loops = Array.from(new Set(Object.values(loopTable)));
+
+	return loops.reduce((stats, loop: number) => {
+		stats[loop] = loopEntries.filter(([path, l]) => l === loop).reduce((t, [path,]) => {
+			t[path.length] = t[path.length] || 0;
+			++t[path.length];
+
+			return t;
+		}, {});
+
+		return stats;
+	}, {});*/
+	return loopEntries.reduce((stats, [path, loop]) => {
+		const pathLen = path.length;
+
+		stats[pathLen] = stats[pathLen] || {};
+		stats[pathLen][loop] = stats[pathLen][loop] || 0;
+		++stats[pathLen][loop];
+
+		return stats;
+	}, {});
+};
+
+const loopStats = loopStatistics(loopTable);
+
+
+//fs.writeFile("./static/cube-loop.json", JSON.stringify(loopTable), error => console.log("output file write finished:", error));
+fs.writeFile("./static/cube-loop.json", JSON.stringify(loopStats), error => console.log("output file write finished:", error));
 
 
 

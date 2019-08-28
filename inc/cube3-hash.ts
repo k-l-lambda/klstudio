7fddcb6e454c9e08eabfa1f149f4a760a58adeab
k-l-lambda <k.l.lambda@gmail.com>
2019-08-28 23:42:29 +0800

    cube3-hash.ts: hashCube added.

--- inc/cube3-hash.ts
+++ inc/cube3-hash.ts
@@ -1,13 +1,40 @@
 
+import { Cube3, TWIST_PERMUTATION_48, permutate } from "../inc/cube3";
+import { cubeLoop } from "../inc/cube3-loop";
+import { cubePartitionCode } from "../inc/cube3-partition";
+
+
+
 const hashLibrary = [];
 const hashIndices = {};
 
 
-const printHash = hash => hash.split("").map(c => c.charCodeAt(0).toString()).join(",");
-const parseHash = source => source.split(",").map(Number).map(c => String.fromCharCode(c)).join("");
+const minCode = cube => {
+	const neighborLoops = Array(12).fill(null).map((_, t) => cubePartitionCode(cube.clone().twist(t)));
+
+	const permutatedCodes = TWIST_PERMUTATION_48.map(permutation => permutate(permutation, neighborLoops).map(loop => String.fromCharCode(loop)).join(""));
+
+	return permutatedCodes.reduce((min: {code: string, index: number}, code, index) => (!min || code < min.code) ? {index, code} : min, null)
+};
 
 
-const loadHashes = (depth, iterator) => {
+const hashCube = cube => {
+	const selfLoop = cubeLoop(cube);
+	const selfPartition = cubePartitionCode(cube);
+	const min = minCode(cube);
+
+	return {
+		hash: String.fromCharCode(selfLoop) + String.fromCharCode(selfPartition) + min.code,
+		index: min.index,
+	};
+};
+
+
+const printHash = (hash: string) => hash.split("").map(c => c.charCodeAt(0).toString()).join(",");
+const parseHash = (source: string) => source.split(",").map(Number).map(c => String.fromCharCode(c)).join("");
+
+
+const loadHashes = (depth: number, iterator: Iterator<string>) => {
 	if (hashLibrary[depth]) {
 		console.warn("hash depth load duplicated:", depth);
 
@@ -39,11 +66,28 @@ const loadHashes = (depth, iterator) => {
 };
 
 
+const solveState = state => {
+	const cube = new Cube3({code: state});
+	const {hash, index} = hashCube(cube);
+
+	if (!hashIndices[hash])
+		return null;
+
+	const twist = TWIST_PERMUTATION_48[index][hashIndices[hash].twist];
+
+	// TODO: failed for state 'EACCEARRAEACEACCAEARAAEACA' (UL)?
+
+	return twist;
+};
+
+
 
 export {
+	hashLibrary,
+	hashIndices,
+	hashCube,
 	printHash,
 	parseHash,
 	loadHashes,
-	hashLibrary,
-	hashIndices,
+	solveState,
 };

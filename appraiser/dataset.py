8f35ae1096f0ace0aa7030164dc86f9bf33ee5ed
k-l-lambda <k.l.lambda@gmail.com>
2020-01-05 22:49:44 +0800

    appraiser/dataset.py created.

---
+++ appraiser/dataset.py
@@ -0,0 +1,55 @@
+
+import os
+import os.path as path
+import pandas as pd
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+
+
+DATASET_PATH = os.environ.get('DATASET_PATH')
+IMAGE_DIMENSION = int(os.environ.get('IMAGE_DIMENSION'))
+
+
+def getDataFrames(splitter):
+	filenames = [f for f in os.listdir(DATASET_PATH) if path.isfile(path.join(DATASET_PATH, f))]
+	filenamesTraining = filter(lambda name: not splitter(name), filenames)
+	filenamesValidation = filter(splitter, filenames)
+
+	dataframe = pd.read_csv('./footages/labels.csv')
+	dataframe['filename'] = [hash + ".jpg" for hash in dataframe.hash]
+
+	trainingData = dataframe[dataframe.filename.isin(filenamesTraining)]
+	validationData = dataframe[dataframe.filename.isin(filenamesValidation)]
+
+	return trainingData, validationData
+
+'''	trainGen = ImageDataGenerator(rescale = 1./255).flow_from_dataframe(trainingData,
+		directory = DATASET_PATH,
+		x_col = 'filename',
+		y_col = 'score',
+		target_size = (IMAGE_DIMENSION, IMAGE_DIMENSION),
+		class_mode = 'other',
+		shuffle = True,
+		)
+
+	validationGen = ImageDataGenerator(rescale = 1./255).flow_from_dataframe(validationData,
+		directory = DATASET_PATH,
+		x_col = 'filename',
+		y_col = 'score',
+		target_size = (IMAGE_DIMENSION, IMAGE_DIMENSION),
+		class_mode = 'other',
+		)
+
+	return trainGen, validationGen
+'''
+
+
+def makeDataGenerator(dataframe, **kwargs):
+	return ImageDataGenerator(rescale = 1./255).flow_from_dataframe(dataframe,
+		directory = DATASET_PATH,
+		x_col = 'filename',
+		y_col = 'score',
+		target_size = (IMAGE_DIMENSION, IMAGE_DIMENSION),
+		class_mode = 'other',
+		**kwargs,
+		)
--- appraiser/training.py
+++ appraiser/training.py
@@ -1,35 +1,15 @@
 
-import os
+#import os
 from dotenv import load_dotenv
-import pandas as pd
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import matplotlib.pyplot as plt
 
 
-load_dotenv(dotenv_path = '../.env.local')
+load_dotenv(dotenv_path = './.env.local')
 load_dotenv()
 
 from models import model
+import dataset
 
-
-
-imageDimension = int(os.environ.get('IMAGE_DIMENSION'))
-
-
-dataframe = pd.read_csv('../footages/labels.csv')
-dataframe['filename'] = [hash + ".jpg" for hash in dataframe.hash]
-
-
-train_image_generator = ImageDataGenerator(rescale = 1./255)
-train_data_gen = train_image_generator.flow_from_dataframe(dataframe,
-	directory = os.environ.get('DATASET_PATH'),
-	x_col = 'filename',
-	y_col = 'score',
-	target_size = (imageDimension, imageDimension),
-	class_mode = 'other',
-	)
-
-sample_images, sample_labels = next(train_data_gen)
 
 
 def plotImages(images_arr):
@@ -42,10 +22,55 @@ def plotImages(images_arr):
 	plt.show()
 
 
+def plotHistory(history):
+	acc = history.history['accuracy']
+	val_acc = history.history['val_accuracy']
+
+	loss = history.history['loss']
+	val_loss = history.history['val_loss']
+
+	epochs_range = range(epochs)
+
+	plt.figure(figsize=(8, 8))
+	plt.subplot(1, 2, 1)
+	plt.plot(epochs_range, acc, label='Training Accuracy')
+	plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+	plt.legend(loc = 'lower right')
+	plt.title('Training and Validation Accuracy')
+
+	plt.subplot(1, 2, 2)
+	plt.plot(epochs_range, loss, label='Training Loss')
+	plt.plot(epochs_range, val_loss, label='Validation Loss')
+	plt.legend(loc = 'upper right')
+	plt.title('Training and Validation Loss')
+	plt.show()
+
+
+batch_size = 128
+epochs = 15
+
+
+trainingData, validationData = dataset.getDataFrames(lambda name: name[0] == 'f')
+
+trainGen = dataset.makeDataGenerator(trainingData, batch_size = batch_size, shuffle = True)
+validationGen = dataset.makeDataGenerator(validationData, batch_size = batch_size)
+
+
+#sample_images, sample_labels = next(validationGen)
 #print('labels:', sample_labels)
 #plotImages(sample_images)
 
 
 model.compile(optimizer = 'adam', loss = 'mean_squared_error', metrics = ['accuracy'])
 
-model.summary()
+#model.summary()
+
+
+history = model.fit_generator(
+	trainGen,
+	steps_per_epoch = len(trainingData.index) // batch_size,
+	epochs = epochs,
+	validation_data = validationGen,
+	validation_steps = len(validationData.index) // batch_size
+)
+plotHistory(history)

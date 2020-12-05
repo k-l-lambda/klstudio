91dd994cafaf26d6a6bdb6dc8373fdc3a22c352a
k-l-lambda <k.l.lambda@gmail.com>
2020-12-05 10:34:08 +0800

    cube3-matrix.vue: refined styles.

--- app/components/cube3-matrix.vue
+++ app/components/cube3-matrix.vue
@@ -1,19 +1,27 @@
 <template>
 	<table class="cube3-matrix" :class="{
-		'hide-corners': !showCorners,
-		'hide-edges': !showEdges,
-		'hide-axes': !showAxes,
-	}">
+			'hide-corners': !showCorners,
+			'hide-edges': !showEdges,
+			'hide-axes': !showAxes,
+		}"
+		@mouseleave="focusColumn = focusRow = null"
+	>
 		<thead>
 			<tr>
-				<th>&times;</th>
-				<th v-for="i of 26" :key="i" :class="{corner: i <= 8, edge: i > 8 && i <= 20, axis: i > 20}">{{labels[i - 1]}}</th>
+				<th class="corner-cell"></th>
+				<th v-for="i of 26" :key="i" :class="{corner: i <= 8, edge: i > 8 && i <= 20, axis: i > 20, focus: i === focusColumn}">{{labels[i - 1]}}</th>
 			</tr>
 		</thead>
 		<tbody v-if="matrix">
-			<tr v-for="j of 26" :key="j" :class="{corner: j <= 8, edge: j > 8 && j <= 20, axis: j > 20}">
+			<tr v-for="j of 26" :key="j"
+				:class="{corner: j <= 8, edge: j > 8 && j <= 20, axis: j > 20, focus: j === focusRow}"
+			>
 				<th class="column">{{labels[j - 1]}}</th>
-				<td v-for="i of 26" :key="i" :class="{corner: i <= 8, edge: i > 8 && i <= 20, axis: i > 20}" v-html="matrix[j - 1][i - 1]"></td>
+				<td v-for="i of 26" :key="i"
+					:class="{corner: i <= 8, edge: i > 8 && i <= 20, axis: i > 20, focus: i === focusColumn}"
+					v-html="matrix[j - 1][i - 1]"
+					@mouseenter="focusColumn = i; focusRow = j"
+				></td>
 			</tr>
 		</tbody>
 	</table>
@@ -29,7 +37,6 @@
 	const CUBE3_POSITION_LABELS_NUMBERS = CUBE3_POSITION_LABELS.split("").map(char => char.charCodeAt(0) - A);
 
 	const CUBE3_POSITION_INDICES = Array(26).fill().map((_, i) => CUBE3_POSITION_LABELS_NUMBERS.indexOf(i)).map(index => index > 12 ? index + 1 : index);
-	console.log("CUBE3_POSITION_INDICES:", CUBE3_POSITION_INDICES);
 
 
 
@@ -58,6 +65,8 @@
 			return {
 				labels: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
 				matrix: null,
+				focusColumn: null,
+				focusRow: null,
 			};
 		},
 
@@ -96,7 +105,7 @@
 				this.matrix = vector.map((index, j) => Array(26).fill().map((_, i) =>
 					i === index ? GREEK_LETTERS[ORIENTATION_GREEK_LETTER_ORDER[this.cube.units[CUBE3_POSITION_INDICES[j]]]] : ""));
 
-				console.log("matrix:", positions, vector, [...this.cube.units]);
+				//console.log("matrix:", positions, vector, [...this.cube.units]);
 			},
 		},
 	};
@@ -107,6 +116,7 @@
 	{
 		text-align: center;
 		font-family: monospace;
+		border-collapse: collapse;
 
 		td, th
 		{
@@ -117,6 +127,43 @@
 		th.column
 		{
 			color: red;
+		}
+
+		tr.focus td, td.focus
+		{
+			background-color: #000f;
+		}
+
+		th
+		{
+			font-weight: normal;
+			font-size: 80%;
+			overflow: hidden;
+		}
+
+		.corner-cell
+		{
+			font-size: 100%;
+			&::before
+			{
+				content: "X";
+				visibility: hidden;
+			}
+		}
+
+		tr.focus th, th.focus
+		{
+			font-weight: bold;
+			font-size: 100%;
+		}
+
+		td
+		{
+			background-color: #000d;
+			color: white;
+			text-shadow: white 0 0 12px;
+			border: #fff1 1px solid;
+			font-weight: bold;
 		}
 	}
 </style>

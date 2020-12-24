cf6eb598f86aa99fa1ff93d2be873d7d23c2211e
k-l-lambda <k.l.lambda@gmail.com>
2020-12-24 22:05:33 +0800

    cube3-player.vue: method of roll added.

--- app/views/static-labeled-cube3.vue
+++ app/views/static-labeled-cube3.vue
@@ -58,34 +58,6 @@
 			async animate () {
 				this.animating = true;
 
-				/*const SEGMENT_DURATION = 1.2e+3;
-
-				let yaw = 0;
-
-				while (this.animating) {
-					if (this.$refs.cube) {
-						yaw += (1 + (Math.random() - 0.5) * (Math.random() - 0.5) * 4) * Math.PI;
-
-						const begin = this.$refs.cube.cubeGroup.quaternion.clone();
-						const target = new THREE.Quaternion()
-							.setFromEuler(new THREE.Euler((Math.random() - 0.5) * 2.4, yaw, (Math.random() - 0.5) * 0.2, "YXZ"));
-
-						const start = Date.now();
-						let now = start;
-						while (now - start < SEGMENT_DURATION) {
-							const progress = (now - start) / SEGMENT_DURATION;
-							const t = 3 * progress ** 2 - 2 * progress ** 3;
-							THREE.Quaternion.slerp(begin, target, this.$refs.cube.cubeGroup.quaternion, t);
-
-							await animationDelay();
-							now = Date.now();
-						}
-
-						await msDelay(400);
-					}
-
-					await animationDelay();
-				}*/
 				await rollQuaternion(this.$refs.cube.cubeGroup.quaternion, {
 					onSegment: () => this.animating,
 					segmentDuration: 1.2e+3,

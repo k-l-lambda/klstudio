9cad3e07f086077e6a2268c10d13b235140aa2ac
k-l-lambda <k.l.lambda@gmail.com>
2021-01-07 21:39:23 +0800

    env.js: enhanced local env file name.

--- env.js
+++ env.js
@@ -10,10 +10,15 @@ import dotenv from "dotenv";
 const appRoot = fs.realpathSync(".");
 
 
+let scriptName = process.argv[process.argv.length - 1];
+scriptName = scriptName && scriptName.match(/([\w_]+)\.\w+$/);
+scriptName = scriptName && scriptName[1];
+
 const dotenvName = path.resolve(appRoot, ".env");
 
 
 const dotenvFiles = [
+	`${dotenvName}.${scriptName}.local`,
 	`${dotenvName}.local`,
 	dotenvName,
 ].filter(Boolean);

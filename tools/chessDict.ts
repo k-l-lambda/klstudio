f1ad0f260fae9bc10e691154335d9bcca34a89a1
k.l.lambda <k.l.lambda@gmail.com>
2021-02-02 09:27:33 +0800

    chessDict.ts: source loading added.

--- tools/chessDict.ts
+++ tools/chessDict.ts
@@ -11,7 +11,7 @@ import {WorkerAgent} from "../inc/chessWorkers";
 
 interface AnalyzationBranch {
 	move: string;
-	value: number;
+	score: number;
 	pv: string;
 };
 
@@ -24,6 +24,7 @@ interface Analyzation {
 
 const depth = argv.depth || 10;
 const multiPV = argv.multiPV || 20;
+const scoreWidth = argv.scoreWidth || 70;
 
 const agent = new WorkerAgent(require("stockfish")());
 agent.setOptions({MultiPV: multiPV});
@@ -42,7 +43,7 @@ const analyzeFEN = async (fen: string): Promise<Analyzation> => {
 		fen,
 		branches: result.pvs.map(item => ({
 			move: item.move.join(""),
-			value: item.scoreCP,
+			score: item.scoreCP,
 			pv: item.pv.map(move => move.join("")).join(" "),
 		})),
 	};
@@ -54,19 +55,52 @@ const main = async () => {
 	await new Promise(resolve => setTimeout(resolve, 10000));
 	//console.log("argv:", argv);
 	let fens = null;
-	const outputFilename = "0.yaml";
+	let outputFilename = "0.yaml";
 
 	const sourceFile = argv._[0];
 	if (sourceFile) {
-		// TODO: load source
+		const index = Number((sourceFile.match(/\d+/) || [0])[0]);
+		outputFilename = `${index + 1}.yaml`;
+
+		const sourceText = await fs.promises.readFile(sourceFile);
+		const source = YAML.parse(sourceText.toString());
+
+		fens = [].concat(...source.map(item => {
+			const game = new Chess(item.fen);
+			const bestScore = item.branches[0].score;
+
+			const fens = [];
+
+			for (const branch of item.branches) {
+				if (branch.score < bestScore - scoreWidth)
+					break;
+
+				const move = {
+					from: branch.move.substr(0, 2),
+					to: branch.move.substr(2, 2),
+					promotion: branch.move.substr(4),
+				};
+				game.move(move);
+
+				fens.push(game.fen());
+				game.undo();
+			}
+
+			return fens;
+		}));
 	}
 	else
 		// use start position as default source
 		fens = [new Chess().fen()];
 
-	const results = await Promise.all(fens.map(analyzeFEN));
+	const results = [];
+	for (const fen of fens) {
+		const analyzation = await analyzeFEN(fen);
+		results.push(analyzation);
+	}
+	//console.log("results:", outputFilename, results);
 
-	await fs.promises.writeFile(path.resolve("./tools/", outputFilename), YAML.stringify(results));
+	await fs.promises.writeFile(path.resolve("./tools/chess-book/", outputFilename), YAML.stringify(results));
 };
 
 main();

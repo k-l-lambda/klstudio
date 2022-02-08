4c0b61cf822ecbbebee0d385d68e6ffbda96c9f1
k-l-lambda <k.l.lambda@gmail.com>
2022-02-08 21:46:05 +0800

    main.ts: https protocol added.

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 
 import * as express from "express";
 import * as http from "http";
+import * as https from "https";
+import * as path from "path";
+import * as fs from "fs";
 
 import "./env.js";
 //import hot from "./hot";
@@ -8,7 +11,16 @@ import "./env.js";
 
 
 
-const development = process.env.NODE_ENV === "development";
+//const development = process.env.NODE_ENV === "development";
+
+
+const HAS_HTTPS = !!process.env.HTTPS;
+
+
+const credentials = HAS_HTTPS && {
+	key: fs.readFileSync(path.resolve("./certificates/key.pem"), "utf8"),
+	cert: fs.readFileSync(path.resolve("./certificates/cert.pem"), "utf8"),
+};
 
 
 /*const simpleTemplate = (script, { title = "", preDoc = "" } = {}) => `
@@ -50,8 +62,17 @@ else {
 
 
 const httpServer = http.createServer(app);
+const httpsServer = HAS_HTTPS && https.createServer(credentials, app);
 
 const port = Number(process.env.PORT);
-httpServer.listen(port, process.env.HOST, () => {
-	console.log("K.L. Studio server online:", `http://${process.env.HOST}:${port}`);
-});
+
+if (HAS_HTTPS) {
+	httpsServer.listen(port, process.env.HOST, () => {
+		console.log("K.L. Studio server online:", `https://${process.env.HOST}:${port}`);
+	});
+}
+else {
+	httpServer.listen(port, process.env.HOST, () => {
+		console.log("K.L. Studio server online:", `http://${process.env.HOST}:${port}`);
+	});
+}

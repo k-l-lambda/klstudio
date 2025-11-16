b07e54ea7eccaeeba6ae82a5d52f6d8913255fac
K.L. Λ <k.l.lambda@gmail.com>
2025-11-17 00:11:14 +0800

    vite.config.mjs: use relative base.

--- vite.config.mjs
+++ vite.config.mjs
@@ -13,6 +13,7 @@ export default defineConfig(({mode}) => {
 	const env = loadEnv(mode, process.cwd(), '');
 
 	return {
+		base: "./",
 		assetsInclude: ["**/*.dat", "**/*.gltf", "**/*.yaml"],
 		plugins: [
 			vue({
